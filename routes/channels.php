a5f905cf823d7ff6e8b5487887bac8a439d25090
daniele-zain <102690703+daniele-zain@users.noreply.github.com>
2024-05-25 18:55:47 +0300

    added Events and Channels routes

---
+++ routes/channels.php
@@ -0,0 +1,15 @@
+<?php
+
+use Illuminate\Support\Facades\Broadcast;
+
+Broadcast::channel('App.Models.User.{id}', function ($user, $id) {
+    return (int) $user->id === (int) $id;
+});
+
+Broadcast::channel('Messenger.{id}', function ($user, $id) {
+    if($user->id == $id){
+
+        return $user;
+    }
+
+});
